ee86a6bc01437c9d3edcfe668adaa2e87c6055cf
MohammadPro <105748037+MohammadPro@users.noreply.github.com>
2024-02-05 21:16:08 +0400

    Add files via upload

---
+++ SearchFragment.java
@@ -0,0 +1,13 @@
+package com.example.myapplication.fragments;
+import androidx.fragment.app.Fragment;
+
+import com.example.myapplication.R;
+
+public class SearchFragment extends Fragment {
+
+
+        public SearchFragment() {
+            super(R.layout.search_fragment);
+        }
+    }
+
